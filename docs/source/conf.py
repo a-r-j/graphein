504aa4bfb4edc99da0c9bcfa28dd45e1905ff643
arian <arjamasb@gmail.com>
2020-07-04 15:23:24 +0100

    [Docs] remove bool autodoc option. Think this is causing issues with RTD

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,8 +35,6 @@ extensions = ['sphinx.ext.autodoc',
 
 
 autodoc_default_options = {
-    'members': True,
-    'member-order': 'bysource',
     'special-members': '__init__',
 }
 # Add any paths that contain templates here, relative to this directory.
