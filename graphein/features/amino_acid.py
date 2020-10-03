1ee87a0dd57645e457c0e19e8acf222871f169ae
a-r-j <arjamasb@gmail.com>
2020-10-03 15:38:08 +0100

    [Features] add meiler embeddings and loading function

--- graphein/features/amino_acid.py
+++ graphein/features/amino_acid.py
@@ -23,6 +23,22 @@ def load_expasy_scales() -> pd.DataFrame:
     return df
 
 
+@lru_cache
+def load_meiler_embeddings() -> pd.DataFrame:
+    """
+    Load pre-downloaded EXPASY scales.
+
+    This helps with node featurization.
+
+    The function is LRU-cached in memory for fast access
+    on each function call.
+    """
+    df = pd.read_csv(
+        Path(__file__).parent / "meiler_embeddings.csv", index_col=0
+    )
+    return df
+
+
 def expasy_protein_scale(n, d) -> pd.Series:
     """
     Return amino acid features that come from the EXPASY protein scale.
@@ -33,5 +49,20 @@ def expasy_protein_scale(n, d) -> pd.Series:
     :param d: NetworkX node attributes.
     """
     df = load_expasy_scales()
+    amino_acid = d["residue_name"]
+    return df[amino_acid]
+
+
+def meiler_embbeding(n, d) -> pd.Series:
+    """
+    Return amino acid features that come from the reduced dimensional embeddings of amino acid physicochemical properties.
+
+    Source: https://link.springer.com/article/10.1007/s008940100038
+    doi: https://doi.org/10.1007/s008940100038
+
+    :param n: Node in a NetworkX graph
+    :param d: NetworkX node attributes.
+    """
+    df = load_meiler_embeddings()
     amino_acid = d["residue_name"]
     return df[amino_acid]
