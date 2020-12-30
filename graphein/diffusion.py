0fbaa99782c811d90eae100d6d585ed80f6d26d3
Eric Ma <ericmajinglong@gmail.com>
2020-12-30 12:36:52 -0500

    Add diffusion matrices code

---
+++ graphein/diffusion.py
@@ -0,0 +1,82 @@
+"""Generate diffusion matrices based on NetworkX graph objects.
+
+Author: Arian Jamasb & Eric Ma
+
+Each of the functions here accepts a NetworkX graph object
+and returns a 2D xarray.
+These arrays can then be stacked to generate a diffusion tensor.
+"""
+import networkx as nx
+import xarray as xr
+
+from .utils import format_adjacency, generate_feature_dataframe
+from .features.edges.distance import compute_distmat
+
+
+def identity_matrix(G: nx.Graph) -> xr.DataArray:
+    """Return the identity diffusion matrix.
+
+    This is nothing more than the identity matrix
+    with diagonals of 1.
+    """
+    return format_adjacency(G, np.eye(len(G)), "identity")
+
+
+def adjacency_matrix_power(
+    G: nx.Graph,
+    amat_kwargs: Dict = {},
+    with_identity: bool = True
+    power: float = 1,
+) -> xr.DataArray:
+    """Return the matrix power of the adjacency matrix.
+
+    :param amat_kwargs: Keyword arguments to configure
+        NetworkX's adjacency_matrix function.
+    :param with_identity: Whether or not to add in the identity matrix
+        to the adjacency matrix.
+        Effectively adding "self loops".
+    :param power: Matrix power to raise the adjacency matrix.
+    """
+    adjacency_matrix_kwargs = {"weight": "weight", "nodelist": None}
+    adjacency_matrix_kwargs.update(amat_kwargs)
+    amat = nx.adjacency_matrix(G, **adjacency_matrix_kwargs)
+    if with_identity:
+        amat = amat + np.eye(len(G))
+    amat = np.linalg.matrix_power(amat, power)
+    return format_adjacency(
+        G,
+        amat,
+        f"adjacency_matrix_power_{power}",
+    )
+
+
+def inverse_distance_matrix(G, power) -> xr.DataArray:
+    """Return the inverse distance matrix.
+
+    Using the coordinates present on the graph object,
+    calculate the inverse power distance matrix.
+
+    The resultant array will be symmetric,
+    with entries [i, j] corresponding to 1 / (distance**power),
+    where distance == euclidean distance between the (x, y, z) coordinates
+    of the two graphs.
+    Diagonals (entries [i, i]) are set to 0.
+
+    :param G: NetworkX Graph object.
+    :param power: The power for the distance calculation.
+    """
+    def extract_coords(n, d):
+        coord_names = ("x_coord", "y_coord", "z_coord")
+        coords = pd.Series({
+            coord_name: d[coord_name] for coord_name in coord_names
+        }, name=n)
+    return coords
+
+    coords = generate_feature_dataframe(G, funcs=[extract_coords])
+    distmat = compute_distmat(coords).values
+
+    I = np.eye(len(G))
+    distmat = (distmat + I) ** power
+    distmat = 1 / distmat
+    distamt -= I
+    return format_adjacency(G, distmat, f"inverse_distance_matrix_power_{power}")
--- graphein/utils.py
+++ graphein/utils.py
@@ -1,3 +1,8 @@
+import networkx as nx
+import pandas as pd
+from typing import List, Callable
+
+
 def onek_encoding_unk(x, allowable_set):
     """
     Function for one hot encoding
@@ -8,3 +13,176 @@ def onek_encoding_unk(x, allowable_set):
     # if x not in allowable_set:
     #    x = allowable_set[-1]
     return [x == s for s in allowable_set]
+
+
+def generate_feature_dataframe(
+    G: nx.Graph,
+    funcs: List[Callable],
+    return_array=False,
+) -> pd.DataFrame:
+    """
+    Return a pandas DataFrame representation of node metadata.
+
+    `funcs` has to be list of callables whose signature is
+
+        f(n, d) -> pd.Series
+
+    where `n` is the graph node,
+    `d` is the node metadata dictionary.
+    The function must return a pandas Series whose name is the node.
+
+    Example function:
+
+    ```python
+    def x_vec(n: Hashable, d: Dict[Hashable, Any]) -> pd.Series:
+        return pd.Series({"x_coord": d["x_coord"]}, name=n)
+    ```
+
+    One fairly strong assumption is that each func
+    has all the information it needs to act
+    stored on the metadata dictionary.
+    If you need to reference an external piece of information,
+    such as a dictionary to look up values,
+    set up the function to accept the dictionary,
+    and use `functools.partial`
+    to "reduce" the function signature to just `(n, d)`.
+    An example below:
+
+    ```python
+    from functools import partial
+    def get_molweight(n, d, mw_dict):
+        return pd.Series({"mw": mw_dict[d["amino_acid"]]}, name=n)
+
+    mw_dict = {"PHE": 165, "GLY": 75, ...}
+    get_molweight_func = partial(get_molweight, mw_dict=mw_dict)
+
+    generate_feature_dataframe(G, [get_molweight_func])
+    ```
+
+    The `name=n` piece is important;
+    the `name` becomes the row index in the resulting dataframe.
+
+    The series that is returned from each function
+    need not only contain one key-value pair.
+    You can have two or more, and that's completely fine;
+    each key becomes a column in the resulting dataframe.
+
+    A key design choice: We default to returning DataFrames,
+    to make inspecting the data easy,
+    but for consumption in tensor libraries,
+    you can turn on returning a NumPy array
+    by switching `return_array` to True.
+
+    ## Parameters
+
+    - `G`: A NetworkX-compatible graph object.
+    - `funcs`: A list of functions.
+    - `return_array`: Whether or not to return
+        a NumPy array version of the data.
+        Useful for consumption in tensor libs, like PyTorch or JAX.
+
+    ## Returns
+
+    - A pandas DataFrame.
+    """
+    matrix = []
+    for n, d in G.nodes(data=True):
+        series = []
+        for func in funcs:
+            res = func(n, d)
+            if res.name != n:
+                raise NameError(
+                    f"function {func.__name__} returns a series "
+                    "that is not named after the node."
+                )
+            series.append(res)
+        matrix.append(pd.concat(series))
+
+    df = pd.DataFrame(matrix)
+    if return_array:
+        return df.values
+    return df
+
+
+def format_adjacency(G: nx.Graph, adj: np.ndarray, name: str) -> xr.DataArray:
+    """
+    Format adjacency matrix nicely.
+
+    Intended to be used when computing an adjacency-like matrix
+    off a graph object G.
+    For example, in defining a func:
+
+    ```python
+    def my_adj_matrix_func(G):
+        adj = some_adj_func(G)
+        return format_adjacency(G, adj, "xarray_coord_name")
+    ```
+
+    ## Assumptions
+
+    1. `adj` should be a 2D matrix of shape (n_nodes, n_nodes)
+    1. `name` is something that is unique amongst all names used
+    in the final adjacency tensor.
+
+    ## Parameters
+
+    - `G`: NetworkX-compatible Graph
+    - `adj`: 2D numpy array
+    - `name`: A unique name for the kind of adjacency matrix
+        being constructed.
+        Gets used in xarray as a coordinate in the "name" dimension.
+
+    ## Returns
+
+    - An XArray DataArray of shape (n_nodes, n_nodes, 1)
+    """
+    expected_shape = (len(G), len(G))
+    if adj.shape != expected_shape:
+        raise ValueError(
+            "Adjacency matrix is not shaped correctly, "
+            f"should be of shape {expected_shape}, "
+            f"instead got shape {adj.shape}."
+        )
+    adj = np.expand_dims(adj, axis=-1)
+    nodes = list(G.nodes())
+    return xr.DataArray(
+        adj,
+        dims=["n1", "n2", "name"],
+        coords={"n1": nodes, "n2": nodes, "name": [name]},
+    )
+
+
+def generate_adjacency_tensor(
+    G: nx.Graph, funcs: List[Callable], return_array=False
+) -> xr.DataArray:
+    """
+    Generate adjacency tensor for a graph.
+
+    Uses the collection of functions in `funcs`
+    to build an xarray DataArray
+    that houses the resulting "adjacency tensor".
+
+    A key design choice:
+    We default to returning xarray DataArrays,
+    to make inspecting the data easy,
+    but for consumption in tensor libraries,
+    you can turn on returning a NumPy array
+    by switching `return_array` to True.
+
+    ## Parameters
+
+    - G: NetworkX Graph.
+    - funcs: A list of functions that take in G
+        and return an xr.DataArray
+
+    ## Returns
+    - xr.DataArray,
+        which is of shape (n_nodes, n_nodes, n_funcs).
+    """
+    mats = []
+    for func in funcs:
+        mats.append(func(G))
+    da = xr.concat(mats, dim="name")
+    if return_array:
+        return da.data
+    return da
