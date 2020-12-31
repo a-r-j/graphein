6b3a86df936ce570792f05d63a9a822fb7bda010
a-r-j <arjamasb@gmail.com>
2020-12-31 04:21:22 +0000

    [Protein meshes] add base protein mesh functionality

---
+++ graphein/protein/meshes.py
@@ -0,0 +1,193 @@
+"""Functions to create protein meshes via pymol."""
+# Graphein
+# Author: Arian Jamasb <arian@jamasb.io>
+# License: MIT
+# Project Website: https://github.com/a-r-j/graphein
+# Code Repository: https://github.com/a-r-j/graphein
+from __future__ import annotations
+
+import importlib.util
+import logging
+from typing import List, NamedTuple, Optional, Tuple
+
+from pytorch3d.structures import Meshes
+
+from graphein.protein.config import ProteinMeshConfig
+
+log = logging.getLogger(__name__)
+
+
+def check_for_pymol_installation() -> None:
+    """Checks for presence of a pymol installation"""
+    spec = importlib.util.find_spec("pymol")
+    if spec is None:
+        log.error(
+            "Please install pymol: conda install -c schrodinger pymol or conda install -c tpeulen pymol-open-source"
+        )
+
+
+def configure_pymol_session(
+    config: Optional[ProteinMeshConfig] = None,
+) -> None:
+    """
+    Configures a PyMol session based on config.parse_pymol_commands. Uses default parameters -"cKq".
+    See: https://pymolwiki.org/index.php/Command_Line_Options
+    :param config: ProteinMeshConfig to use
+    :return: None
+    """
+    from ipymol import viewer as pymol
+
+    # If no config is provided, use default
+    if config is None:
+        config = ProteinMeshConfig()
+
+    # Start PyMol session
+    pymol.start([config.pymol_command_line_options])
+
+
+def get_obj_file(
+    pdb_file: Optional[str] = None,
+    pdb_code: Optional[str] = None,
+    out_dir: Optional[str] = None,
+    config: Optional[ProteinMeshConfig] = None,
+) -> str:
+    """
+    Runs PyMol to compute surface/mesh for a given protein
+    :param pdb_file:  path to pdb_file to use
+    :param pdb_code: 4-letter pdb accession code
+    :param out_dir: path to output. Defaults to /tmp/
+    :param config: ProteinMeshConfig containing pymol commands to run. Default is "show surface"
+    :return: returns path to .obj file (str)
+    """
+    from ipymol import viewer as pymol
+
+    check_for_pymol_installation()
+
+    # Check inputs
+    if not pdb_code and not pdb_file:
+        raise ValueError("Please pass either a pdb_file or pdb_code argument")
+    if pdb_code and pdb_file:
+        raise ValueError(
+            "Please pass either a pdb_file or pdb_code argument. Not both"
+        )
+
+    if out_dir is None:
+        out_dir = "/tmp/"
+
+    configure_pymol_session()
+
+    # Load structure
+    pymol.load(pdb_file) if pdb_file else pymol.fetch(pdb_code)
+    # Create file_name
+    file_name = (
+        pdb_file[:-3] + "obj" if pdb_file else out_dir + pdb_code + ".obj"
+    )
+
+    if config is None:
+        config = ProteinMeshConfig()
+
+    commands = parse_pymol_commands(config)
+    print(commands)
+    run_pymol_commands(commands)
+
+    # Save surface object as .obj
+    pymol.do(f"save {file_name}")
+    log.debug(f"Saved {file_name}")
+    return file_name
+
+
+def parse_pymol_commands(config: ProteinMeshConfig) -> List[str]:
+    """
+    Parses pymol commands from config. At the moment users can only supply a list of string commands.
+    :param config:
+    :return: list of pymol commands to run
+    """
+    if config is None:
+        config = ProteinMeshConfig()
+
+    # Todo parsing of individual pymol mesh calculation parameters. There's a lot of them so this is not a priority now.
+    if config.pymol_commands is not None:
+        return config.pymol_commands
+
+
+def run_pymol_commands(commands: List[str]) -> None:
+    """
+    Runs Pymol Commands
+    :param commands::
+    :return: None
+    """
+    from ipymol import viewer as pymol
+
+    for c in commands:
+        log.debug(c)
+        pymol.do(c)
+
+
+def create_mesh(
+    pdb_file: Optional[str] = None,
+    pdb_code: Optional[str] = None,
+    out_dir: Optional[str] = None,
+    config: Optional[ProteinMeshConfig] = None,
+) -> Tuple[torch.FloatTensor, NamedTuple, NamedTuple]:
+    """
+    Creates a PyTorch 3D mesh from a pdb_file or pdb code.
+    :param pdb_file: path to pdb pdb_file
+    :param pdb_code: 4-letter PDB accession code
+    :param out_dir: output directory to store ".obj" file. Defaults to /tmp/
+    :param config:  ProteinMeshConfig config to use. Defaults to default config in graphein.protein.config
+    :return: verts, faces, aux
+    """
+    from pytorch3d.io import load_obj
+
+    if config is None:
+        config = ProteinMeshConfig()
+
+    obj_file = get_obj_file(
+        pdb_code=pdb_code, pdb_file=pdb_file, out_dir=out_dir, config=config
+    )
+    verts, faces, aux = load_obj(obj_file)
+    return verts, faces, aux
+
+
+def normalize_and_center_mesh_vertices(verts: torch.FloatTensor):
+    """
+    We scale normalize and center the target mesh to fit in a sphere of radius 1 centered at (0,0,0).
+    (scale, center) will be used to bring the predicted mesh to its original center and scale
+    Note that normalizing the target mesh, speeds up the optimization but is not necessary!
+    :param verts: mesh vertices
+    :return: normalized and centered vertices
+    """
+    center = verts.mean()
+    verts = verts - center
+    scale = max(verts.abs().max(0)[0])
+    verts = verts / scale
+    return verts
+
+
+def convert_verts_and_face_to_mesh(
+    verts: torch.FloatTensor, faces: NamedTuple
+) -> Meshes:
+    """
+    Converts vertices and faces into a pytorch3d.structures Meshes object.
+    :param verts: vertices
+    :param faces: faces
+    :return: Meshes object
+    """
+    faces_idx = faces.verts_idx
+    return Meshes(verts=[verts], faces=[faces_idx])
+
+
+if __name__ == "__main__":
+    from graphein.protein.visualisation import plot_pointcloud
+
+    config = ProteinMeshConfig(
+        pymol_commands=[
+            "show surface",
+            "set surface_solvent, on",
+            "set solvent_radius, 10000",
+        ]
+    )
+    verts, faces, aux = create_mesh(pdb_code="3eiy", config=config)
+
+    trg_mesh = convert_verts_and_face_to_mesh(verts, faces)
+    plot_pointcloud(trg_mesh)
