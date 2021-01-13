b167df4b2a431b5d5f8cdaf1d79febb3b4dbe9d7
a-r-j <arjamasb@gmail.com>
2021-01-13 11:06:24 +0000

    fix imports

--- graphein/protein/__init__.py
+++ graphein/protein/__init__.py
@@ -1,10 +1,18 @@
-from graphs import *
-from meshes import *
-from resi_atoms import *
+from .graphs import *
+from .meshes import *
+from .resi_atoms import *
 
-from utils import *
-from visualisation import *
+from .utils import *
+from .visualisation import plot_protein_structure_graph
+
+try:
+    from .visualisation import plot_pointcloud
+    from .meshes import * 
+except ImportError:
+    pass
 
 from .config import *
 from .edges import *
 from .features import *
+from .graphs import *
+from .utils import *
