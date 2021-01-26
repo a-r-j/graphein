75a7b520e4ae0a5eec723712c0ba9503e4958bb5
Eric Ma <ericmajinglong@gmail.com>
2021-01-25 21:57:25 -0500

    First suite of tests

---
+++ tests/protein/test_graphs.py
@@ -0,0 +1,24 @@
+"""Tests for graphein.protein.graphs"""
+
+from graphein.protein.graphs import construct_graph
+import networkx as nx
+from pathlib import Path
+
+
+def test_construct_graph():
+    """Example-based test that graph construction works correctly.
+
+    Uses 4hhb PDB file as an example test case.
+    """
+    file_path = Path(__file__).parent / "test_data/4hhb.pdb"
+    G = construct_graph(pdb_path=str(file_path))
+    assert isinstance(G, nx.Graph)
+    assert len(G) == 574
+
+    # Check number of peptide bonds
+    peptide_bond_edges = [
+        (u, v)
+        for u, v, d in G.edges(data=True)
+        if d["kind"] == {"peptide_bond"}
+    ]
+    assert len(peptide_bond_edges) == 570
