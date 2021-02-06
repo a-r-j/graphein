90cd915d26982ec835cfcfe2a06eb6815d168e00
a-r-j <arjamasb@gmail.com>
2021-02-06 19:32:26 +0000

    clean up amino acid feats

--- graphein/protein/features/nodes/amino_acid.py
+++ graphein/protein/features/nodes/amino_acid.py
@@ -72,36 +72,5 @@ def meiler_embedding(n, d) -> pd.Series:
     return df[amino_acid]
 
 
-def aaindex_1_feat(n, d, feature_name: str) -> pd.Series:
-    # TODO: This function needs a docstring.
-
-    # TODO: Decide whether these imports should go at the top of the file or not.
-    # If not, document why they are imported inside the function,
-    # as this is not a common practice.
-    # Best to minimize the number of optional dependencies.
-    from Bio.PDB.Polypeptide import three_to_one
-    from propy.AAIndex import GetAAIndex1
-
-    df = GetAAIndex1(feature_name)
-    df = pd.Series(df).loc[three_to_one(d["residue_name"])]
-    return df
-
-
-def load_feature_dataframe(n, d) -> pd.Series:
-    """
-    Generic function for loading features from an on-disk file
-    :param n:
-    :param d:
-    :return:
-    """
-    # TODO: Is this function still needed?
-    raise NotImplementedError
-
-
 def load_esm_embedding_residue(n, d) -> pd.Series:
     raise NotImplementedError
-
-
-# TODO: Remove this from the script, as this should be instead present as a test.
-if __name__ == "__main__":
-    aaindex_1_feat(feature_name="KRIW790103")
