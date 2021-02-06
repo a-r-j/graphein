943c23b889f78bc6412b4943c67b2ea099acf56f
a-r-j <arjamasb@gmail.com>
2021-02-06 19:33:41 +0000

    clean up dssp

--- graphein/protein/features/nodes/dssp.py
+++ graphein/protein/features/nodes/dssp.py
@@ -66,38 +66,3 @@ def add_dssp_features(G: nx.Graph) -> nx.Graph:
     G.graph["dssp_exposure_rsa"] = dssp_df["exposure_rsa"]
     G.graph["dssp_exposure_asa"] = dssp_df["exposure_asa"]
     return G
-
-
-# TODO: decide whether this needs to be deleted or not.
-"""
-def _get_protein_features(
-        self, pdb_code: Optional[str], file_path: Optional[str], chain_selection: str
-) -> pd.DataFrame:
-    :param file_path: (str) file path to PDB file
-    :param pdb_code: (str) String containing four letter PDB accession
-    :return df (pd.DataFrame): Dataframe containing output of DSSP (Solvent accessibility, secondary structure for each residue)
-
-    # Run DSSP on relevant PDB file
-    if pdb_code:
-        d = dssp_dict_from_pdb_file(self.pdb_dir + pdb_code + ".pdb")
-    if file_path:
-        d = dssp_dict_from_pdb_file(file_path)
-
-    # Subset dataframe to those in chain_selection
-    if chain_selection != "all":
-        df = df.loc[df["chain"].isin(chain_selection)]
-    # Rename cysteines to 'C'
-    df["aa"] = df["aa"].str.replace("[a-z]", "C")
-    df = df[df["aa"].isin(list(aa1))]
-
-    # Drop alt_loc residues
-    df = df.loc[df["icode"] == " "]
-
-    # Add additional Columns
-    df["aa_three"] = df["aa"].apply(one_to_three)
-    df["max_acc"] = df["aa_three"].map(residue_max_acc["Sander"].get)
-    df[["exposure_rsa", "max_acc"]] = df[["exposure_rsa", "max_acc"]].astype(float)
-    df["exposure_asa"] = df["exposure_rsa"] * df["max_acc"]
-    df["index"] = df["chain"] + ":" + df["aa_three"] + ":" + df["resnum"].apply(str)
-    return df
-"""
