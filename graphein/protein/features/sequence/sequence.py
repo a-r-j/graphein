1c360b55307e770adf7b119c67d4597faa756a2f
a-r-j <arjamasb@gmail.com>
2021-02-08 17:35:19 +0000

    black

--- graphein/protein/features/sequence/sequence.py
+++ graphein/protein/features/sequence/sequence.py
@@ -27,7 +27,6 @@ def molecular_weight(protein: str, seq_type: str = "protein"):
     return func(protein)
 
 
-
 @dispatch(nx.Graph, str)
 def molecular_weight(protein: nx.Graph, seq_type: str = "protein"):
     func = partial(SeqUtils.molecular_weight, seq_type=seq_type)
