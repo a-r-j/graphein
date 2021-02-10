1e3beae13ffd260b9fa1a5d9ac4a00b85a2ecf7e
cch1999 <charlieharris001@gmail.com>
2021-02-10 10:49:37 +0000

    Updated pscdb example to 2.0

--- datasets/pscdb/make_rearrangement_data.py
+++ datasets/pscdb/make_rearrangement_data.py
@@ -4,9 +4,19 @@ import pandas as pd
 import torch as torch
 from tqdm import tqdm
 
-from graphein.construct_graphs import ProteinGraph
+from graphein.protein.graphs import construct_graph
+from graphein.protein.config import ProteinGraphConfig, DSSPConfig, GetContactsConfig
+from graphein.protein.edges.intramolecular import hydrophobic, 
+                                                salt_bridge,
+                                                    hydrogen_bond, 
+                                                    t_stacking, 
+                                                    van_der_waals, 
+                                                    pi_cation, 
+                                                    pi_stacking,
+                                                    t_stacking
 
 if __name__ == "__main__":
+    """
     parser = argparse.ArgumentParser(
         description="Pass Graph Construction Params"
     )
@@ -19,6 +29,7 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
     # attention = args.attention
+    """
 
     # Load Dataframe
     # df = pd.read_feather('structural_rearrangement_data.feather')
@@ -26,20 +37,30 @@ if __name__ == "__main__":
     print(df)
 
     # Initialise Graph Constructor
-    pg = ProteinGraph(
-        granularity="CA",
-        insertions=False,
-        keep_hets=False,
-        node_featuriser="meiler",
-        get_contacts_path=args.get_contacts_path,
-        pdb_dir="pdbs/",
-        contacts_dir="contacts/",
-        exclude_waters=True,
-        covalent_bonds=False,
-        include_ss=args.include_ss,
-        include_ligand=False,
-        edge_distance_cutoff=args.edge_distance_cutoff,
-    )
+    configs = {
+        "granularity": "CA",
+        "keep_hets": False,
+        "insertions": False,
+        "verbose": False,
+        "pdb_dir": "../examples/pdbs/",
+        "get_contacts_config": GetContactsConfig(contacts_dir="../examples/contacts/",
+                                                pdb_dir="../examples/contacts/"),
+        "dssp_config": DSSPConfig(),
+    }
+
+    config = ProteinGraphConfig(**configs)
+
+    config.edge_construction_functions = [
+        salt_bridge, 
+        hydrogen_bond, 
+        van_der_waals, 
+        pi_cation, 
+        pi_stacking,
+        hydrophobic,
+        t_stacking
+    ]
+    # Test High-level API
+
 
     # Iterate over rows to produce Graph, pickle graph and label
     for row in tqdm(range(len(df))):
@@ -47,10 +68,10 @@ if __name__ == "__main__":
         file_path = f'pdbs/{example["Free PDB"]}.pdb'
         contact_file = f'contacts/{example["Free PDB"]}_contacts.tsv'
 
-        g = pg.dgl_graph_from_pdb_code(
-            pdb_code=example["Free PDB"],
-            chain_selection=list(example["Free Chains"]),
-        )
+        g = construct_graph(
+                config=config,
+                pdb_code=example["Free PDB"])
+
 
         print(g)
 
