81668377a619a6ca66f3b3e1d17aa460a8d85070
a-r-j <arjamasb@gmail.com>
2022-02-09 13:22:37 +0100

    isort

--- graphein/protein/__init__.py
+++ graphein/protein/__init__.py
@@ -4,9 +4,9 @@ from .edges import *
 from .features import *
 from .graphs import *
 from .resi_atoms import *
+from .subgraphs import *
 from .utils import *
 from .visualisation import plot_protein_structure_graph
-from .subgraphs import *
 
 try:
     from .meshes import *
