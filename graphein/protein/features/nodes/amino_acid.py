4e2164e297419c45f65db693616136ee8b6a088a
a-r-j <arjamasb@gmail.com>
2022-02-28 01:46:48 +0100

    Adds one-hot encoding of amino acids types as node feature

--- graphein/protein/features/nodes/amino_acid.py
+++ graphein/protein/features/nodes/amino_acid.py
@@ -8,10 +8,13 @@
 import logging
 from functools import lru_cache
 from pathlib import Path
-from typing import List, Optional, Union
+from typing import Any, Dict, List, Optional, Union
 
 import numpy as np
 import pandas as pd
+
+from graphein.protein.resi_atoms import BASE_AMINO_ACIDS, RESI_THREE_TO_1
+from graphein.utils.utils import onek_encoding_unk
 
 log = logging.getLogger(__name__)
 
@@ -123,4 +126,40 @@ def meiler_embedding(
 
     d["meiler"] = features
 
+    return features
+
+
+def amino_acid_one_hot(
+    n,
+    d: Dict[str, Any],
+    return_array=True,
+    allowable_set: Optional[List[str]] = None,
+) -> Union[pd.Series, np.ndarray]:
+    """Adds a one-hot encoding of amino acid types as a node attribute.
+
+    :param n: node name, this is unused and only included for compatibility with the other functions
+    :type n: str
+    :param d: Node data
+    :type d: Dict[str, Any]
+    :param return_array: If True, returns a numpy array of one-hot encoding, otherwise returns a pd.Series. Default is True.
+    :type return_array: bool
+    :param allowable_set: Specifies vocabulary of amino acids. Default is None (which uses `graphein.protein.resi_atoms.STANDARD_AMINO_ACIDS`).
+    :return: One-hot encoding of amino acid types
+    :rtype: Union[pd.Series, np.ndarray]
+    """
+
+    if allowable_set is None:
+        allowable_set = BASE_AMINO_ACIDS
+
+    features = onek_encoding_unk(
+        RESI_THREE_TO_1[d["residue_name"]], allowable_set
+    )
+
+    if return_array:
+        features = np.array(features).astype(int)
+    else:
+        features = pd.Series(features).astype(int)
+        features.index = allowable_set
+
+    d["amino_acid_one_hot"] = features
     return features
