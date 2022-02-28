2a645b2f4246e946c520b6ec5ee3b40a3a2e64de
ricomnl <rmeinl97@googlemail.com>
2022-02-28 17:57:11 +0000

    Add CLI and yaml config parser (#113)
    * Added cli and yml parser
    
    * Added cli and yml parser
    
    * Update base dependencies for CLI
    
    * Added docstrings
    
    * Update changelog
    
    * added documentation
    
    * Added documentation for cli and yaml parser
    
    * Update CONTRIBUTORS.md
    
    Co-authored-by: Rico Meinl <rmeinl97@gmail.com>
    Co-authored-by: Arian Jamasb <arjamasb@gmail.com>

---
+++ tests/utils/test_cli.py
@@ -0,0 +1,35 @@
+"""Tests for graphein.cli"""
+import tempfile
+from pathlib import Path
+
+import networkx as nx
+from click.testing import CliRunner
+
+from graphein.cli import main
+
+DATA_PATH = Path(__file__).resolve().parent
+
+
+def test_cli():
+    """Example-based test that graph construction works correctly.
+
+    Uses 4hhb PDB file as an example test case.
+    """
+    file_path = DATA_PATH.parent / "protein/test_data/4hhb.pdb"
+    temp_dir = tempfile.TemporaryDirectory()
+
+    runner = CliRunner()
+    result = runner.invoke(main, ["-p", file_path, "-o", Path(temp_dir.name)])
+    assert result.exit_code == 0
+
+    G = nx.read_gpickle(Path(temp_dir.name) / f"{file_path.stem}.pickle")
+    assert isinstance(G, nx.Graph)
+    assert len(G) == 574
+    # Check number of peptide bonds
+    peptide_bond_edges = [
+        (u, v)
+        for u, v, d in G.edges(data=True)
+        if d["kind"] == {"peptide_bond"}
+    ]
+    assert len(peptide_bond_edges) == 570
+    temp_dir.cleanup()
