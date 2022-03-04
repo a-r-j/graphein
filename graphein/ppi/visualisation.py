82e21a7d662ea601b9c4947a2e917d95924aca92
a-r-j <arjamasb@gmail.com>
2022-03-04 01:08:14 +0100

    [ppi] improve visualisation functions

--- graphein/ppi/visualisation.py
+++ graphein/ppi/visualisation.py
@@ -48,7 +48,10 @@ def plotly_ppi_graph(
     show_labels: bool = False,
     node_size_multiplier: float = 5.0,
     node_colourscale: str = "Viridis",
-    edge_colours: List[str] = None,
+    edge_colours: Optional[List[str]] = None,
+    edge_opacity: float = 0.5,
+    height: int = 500,
+    width: int = 500,
 ):
     """Plots a PPI graph.
 
@@ -67,6 +70,14 @@ def plotly_ppi_graph(
         'Reds' | 'Blues' | 'Picnic' | 'Rainbow' | 'Portland' | 'Jet' |
         'Hot' | 'Blackbody' | 'Earth' | 'Electric' | 'Viridis' |
     :type node_colourscale: str
+    :param edge_colours: List of colours to use for edges. Default is None (red - string, blue - biogrid, yellow - both).
+    :type edge_colours: List[str], optional
+    :param edge_opacity: Opacity of edges. Default is 0.5.
+    :type edge_opacity: float
+    :param height: Height of the plot. Default is 500.
+    :type height: int
+    :param width: Width of the plot. Default is 500.
+    :type width: int
     :return: Plotly figure of PPI Network
     :rtype: go.Figure
     """
@@ -77,7 +88,7 @@ def plotly_ppi_graph(
 
     # Get node and edge traces
     node_trace = get_node_trace(g, node_size_multiplier, node_colourscale)
-    edge_trace = get_edge_trace(g, edge_colours)
+    edge_trace = get_edge_trace(g, edge_colours, edge_opacity)
     traces = [node_trace] + edge_trace
 
     # Get node labels if using them.
@@ -99,7 +110,8 @@ def plotly_ppi_graph(
             title=title,
             titlefont_size=16,
             showlegend=False,
-            width=500,
+            width=width,
+            height=height,
             hovermode="closest",
             margin=dict(b=20, l=5, r=5, t=40),
             xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
@@ -158,7 +170,9 @@ def get_node_trace(
 
 
 def get_edge_trace(
-    g: nx.Graph, edge_colours: Optional[List[str]] = None
+    g: nx.Graph,
+    edge_colours: Optional[List[str]] = None,
+    edge_opacity: float = 0.5,
 ) -> List[go.Scatter]:
     """Gets edge traces from PPI graph. Returns a list of traces enabling edge colours to be set individually.
 
@@ -183,7 +197,7 @@ def get_edge_trace(
             colour = edge_colours[2]
 
         edge_trace = go.Scatter(
-            line=dict(width=2, color=colour),
+            line=dict(width=2, color=colour, opacity=edge_opacity),
             hoverinfo="text",
             x=(x0, x1),
             y=(y0, y1),
