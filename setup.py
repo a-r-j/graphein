c7922491b7b2c7032d2e020b529a961a313ed205
a-r-j <arjamasb@gmail.com>
2022-04-05 15:17:00 +0100

    bump version to 1.3.0

--- setup.py
+++ setup.py
@@ -136,7 +136,7 @@ class UploadCommand(Command):
 
 setup(
     name="graphein",
-    version="1.2.1",
+    version="1.3.0",
     # versioneer.get_version(),
     # cmdclass=versioneer.get_cmdclass(),
     description="Protein & Interactomic Graph Construction for Machine Learning",
