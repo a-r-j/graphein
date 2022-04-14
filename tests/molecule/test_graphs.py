9ef623c19d671f501102531e1d9d26130ae324f4
Arian Jamasb <arjamasb@gmail.com>
2022-04-14 15:40:44 +0100

    lint tests

--- tests/molecule/test_graphs.py
+++ tests/molecule/test_graphs.py
@@ -1,28 +1,43 @@
-import sys
-sys.path.append('/data/yuanqi/graphein')
+from pathlib import Path
 
-from graphein.molecule.graphs import construct_graph
 from graphein.molecule.config import MoleculeGraphConfig
+from graphein.molecule.graphs import construct_graph
 
 config = MoleculeGraphConfig()
 
+
 def test_generate_graph_sdf():
-    g = construct_graph(config=config, sdf_path="/data/yuanqi/graphein/tests/molecule/test_data/long_test.sdf")
-    return g 
+    g = construct_graph(
+        config=config,
+        sdf_path=str(
+            (Path(__file__).parent / "test_data" / "long_test.sdf").resolve()
+        ),
+    )
+    return g
+
 
 def test_generate_graph_smiles():
-    g = construct_graph(config=config, smiles="N[C@H](CCCc1ccc(N(CCCl)CCCl)cc1)C(=O)O")
-    return g 
+    g = construct_graph(
+        config=config, smiles="N[C@H](CCCc1ccc(N(CCCl)CCCl)cc1)C(=O)O"
+    )
+    return g
+
 
 def test_generate_graph_mol2():
-    g = construct_graph(config=config, mol2_path="/data/yuanqi/graphein/tests/molecule/test_data/short_test.mol2")
-    return g 
+    g = construct_graph(
+        config=config,
+        mol2_path=str(
+            (Path(__file__).parent / "test_data" / "short_test.mol2").resolve()
+        ),
+    )
+    return g
+
 
 def test_generate_graph_pdb():
-    g = construct_graph(config=config, pdb_path="/data/yuanqi/graphein/tests/molecule/test_data/long_test.pdb")
-    return g 
-
-test_generate_graph_sdf()
-test_generate_graph_smiles()
-test_generate_graph_mol2()
-test_generate_graph_pdb()
+    g = construct_graph(
+        config=config,
+        pdb_path=str(
+            (Path(__file__).parent / "test_data" / "long_test.pdb").resolve()
+        ),
+    )
+    return g
