9a529567ddd783598cb2301bcb0869566b860c43
Arian Jamasb <arjamasb@gmail.com>
2022-04-15 15:07:20 +0100

    add more RDKit constants

--- graphein/molecule/atoms.py
+++ graphein/molecule/atoms.py
@@ -14,6 +14,14 @@ from __future__ import annotations
 
 from typing import Dict, List
 
+from graphein.utils.utils import import_message
+
+try:
+    import rdkit.Chem as Chem
+except ImportError:
+    import_message("graphein.molecule.atoms", "rdkit", "rdkit", True)
+
+
 BASE_ATOMS: List[str] = [
     "C",
     "H",
@@ -103,10 +111,98 @@ ALLOWED_BOND_TYPES: List[Chem.BondType] = [
 ]
 """Vocabulary of allowed bondtypes."""
 
-BONDTYPE_TO_CHANNEL: Dict[Chem.BondType, int] = {
+ALLOWED_BOND_TYPE_TO_CHANNEL: Dict[Chem.BondType, int] = {
     Chem.BondType.SINGLE: 0,
     Chem.BondType.DOUBLE: 1,
     Chem.BondType.TRIPLE: 2,
     Chem.BondType.AROMATIC: 3,
 }
 """Mapping of bondtypes to integer values."""
+
+
+ALL_BOND_TYPES: List[Chem.BondType] = [
+    Chem.BondType.AROMATIC,
+    Chem.BondType.DATIVE,
+    Chem.BondType.DATIVEL,
+    Chem.BondType.DATIVER,
+    Chem.BondType.DOUBLE,
+    Chem.BondType.FIVEANDHALF,
+    Chem.BondType.FOURANDHALF,
+    Chem.BondType.HEXTUPLE,
+    Chem.BondType.HYDROGEN,
+    Chem.BondType.IONIC,
+    Chem.BondType.ONEANDHALF,
+    Chem.BondType.OTHER,
+    Chem.BondType.QUADRUPLE,
+    Chem.BondType.QUINTUPLE,
+    Chem.BondType.SINGLE,
+    Chem.BondType.THREEANDHALF,
+    Chem.BondType.THREECENTER,
+    Chem.BondType.TRIPLE,
+    Chem.BondType.TWOANDHALF,
+    Chem.BondType.UNSPECIFIED,
+    Chem.BondType.ZERO,
+]
+"""Vocabulary of all RDkit BondTypes."""
+
+
+ALL_BOND_TYPES_TO_CHANNEL: Dict[Chem.BondType, int] = {
+    Chem.BondType.AROMATIC: 0,
+    Chem.BondType.DATIVE: 1,
+    Chem.BondType.DATIVEL: 2,
+    Chem.BondType.DATIVER: 3,
+    Chem.BondType.DOUBLE: 4,
+    Chem.BondType.FIVEANDHALF: 5,
+    Chem.BondType.FOURANDHALF: 6,
+    Chem.BondType.HEXTUPLE: 7,
+    Chem.BondType.HYDROGEN: 8,
+    Chem.BondType.IONIC: 9,
+    Chem.BondType.ONEANDHALF: 10,
+    Chem.BondType.OTHER: 11,
+    Chem.BondType.QUADRUPLE: 12,
+    Chem.BondType.QUINTUPLE: 13,
+    Chem.BondType.SINGLE: 14,
+    Chem.BondType.THREEANDHALF: 15,
+    Chem.BondType.THREECENTER: 16,
+    Chem.BondType.TRIPLE: 17,
+    Chem.BondType.TWOANDHALF: 18,
+    Chem.BondType.UNSPECIFIED: 19,
+    Chem.BondType.ZERO: 20,
+}
+"""Vocabulary of all RDkit BondTypes mapped to integer values."""
+
+ALL_STEREO_TYPES: List[Chem.rdchem.BondStereo] = [
+    Chem.rdchem.BondStereo.STEREOANY,
+    Chem.rdchem.BondStereo.STEREOCIS,
+    Chem.rdchem.BondStereo.STEREOE,
+    Chem.rdchem.BondStereo.STEREONONE,
+    Chem.rdchem.BondStereo.STEREOTRANS,
+    Chem.rdchem.BondStereo.STEREOZ,
+]
+"""Vocabulary of all RDKit bond stereo types."""
+
+ALL_STEREO_TO_CHANNEL: Dict[Chem.rdchem.BondStereo, int] = {
+    Chem.rdchem.BondStereo.STEREOANY: 0,
+    Chem.rdchem.BondStereo.STEREOCIS: 1,
+    Chem.rdchem.BondStereo.STEREOE: 2,
+    Chem.rdchem.BondStereo.STEREONONE: 3,
+    Chem.rdchem.BondStereo.STEREOTRANS: 4,
+    Chem.rdchem.BondStereo.STEREOZ: 5,
+}
+"""Vocabulary of all RDKit bond stereo types mapped to integer values."""
+
+CHIRAL_TYPE: List[Chem.rdchem.ChiralType] = [
+    Chem.ChiralType.CHI_OTHER,
+    Chem.ChiralType.CHI_TETRAHEDRAL_CCW,
+    Chem.ChiralType.CHI_TETRAHEDRAL_CW,
+    Chem.ChiralType.CHI_UNSPECIFIED,
+]
+"""Vocabulary of all RDKit chiral types."""
+
+CHIRAL_TYPE_TO_CHANNEL: Dict[Chem.rdchem.ChiralType] = {
+    Chem.ChiralType.CHI_OTHER: 0,
+    Chem.ChiralType.CHI_TETRAHEDRAL_CCW: 1,
+    Chem.ChiralType.CHI_TETRAHEDRAL_CW: 2,
+    Chem.ChiralType.CHI_UNSPECIFIED: 3,
+}
+"""Vocabulary of all RDKit chiral types mapped to integer values."""
