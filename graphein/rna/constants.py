a6a220809847579ed312298cc2dca2a6120370b4
Arian Jamasb <arjamasb@gmail.com>
2022-04-27 13:58:04 +0100

    add RNA 3D constants

--- graphein/rna/constants.py
+++ graphein/rna/constants.py
@@ -84,3 +84,157 @@ SS_BOND_TYPES: List[str] = [
     "pseudoknot",
 ]
 """List of valid secondary structure bond types."""
+
+RNA_ATOMS: List[str] = [
+    "C1'",
+    "C2",
+    "C2'",
+    "C3'",
+    "C4",
+    "C4'",
+    "C5",
+    "C5'",
+    "C6",
+    "C8",
+    "N1",
+    "N2",
+    "N3",
+    "N4",
+    "N6",
+    "N7",
+    "N9",
+    "O2",
+    "O2'",
+    "O3'",
+    "O4",
+    "O4'",
+    "O5'",
+    "O6",
+    "OP1",
+    "OP2",
+    "P",
+]
+"""List of valid RNA atoms found in PDB structures."""
+
+RNA_ATOMIC_RADII: Dict[str, float] = {
+    "Csb": 0.77,
+    "Cres": 0.72,
+    "Cdb": 0.67,
+    "Nsb": 0.70,
+    "Ndb": 0.62,
+    "H": 0.37,
+    "Osb": 0.67,
+    "Odb": 0.60,
+    "P": 0.92,
+}
+"""List of atomic radii (in angstroms) from Heyrovska 2008."""
+
+PHOSPHORIC_ACID_BOND_STATE: Dict[str, str] = {
+    "P": "P",
+    "OP1": "Osb",  # TODO check this? Maybe incorrect
+    "OP2": "Odb",  # TODO check this? Maybe incorrect
+}
+
+RIBOSE_BOND_STATE: Dict[str, str] = {
+    "C1'": "Cres",
+    "O1'": "Osb",
+    "C2'": "Cres",
+    "O2'": "Osb",
+    "C3'": "Cres",
+    "O3'": "Osb",
+    "C4'": "Cres",
+    "O4'": "Osb",
+    "C5'": "Cres",
+    "O5'": "Osb",
+    "H": "H",
+}
+
+RNA_BACKBONE_BOND_STATE: Dict[str, str] = {
+    **RIBOSE_BOND_STATE,
+    **PHOSPHORIC_ACID_BOND_STATE,
+}
+
+DEOXYRIBOSE_BOND_STATE: Dict[str, str] = {
+    "C1'": "Cres",
+    "O1'": "Osb",
+    "C2'": "Cres",
+    "O2'": "Osb",
+    "C3'": "Cres",
+    "O3'": "Osb",
+    "C4'": "Cres",
+    "O4'": "Osb",
+    "C5'": "Cres",
+    "O5'": "Osb",
+    "H": "H",
+}
+
+
+RNA_ATOM_BOND_STATE: Dict[str, Dict[str, str]] = {
+    "A": {
+        "N1": "Ndb",
+        "C2": "Cres",
+        "N3": "Ndb",
+        "C4": "Cres",
+        "C5": "Cdb",
+        "C6": "Cres",
+        "N6": "Nsb",
+        "N7": "Ndb",
+        "C8": "Cdb",
+        "N9": "Nsb",
+        "H": "H",
+        **RNA_BACKBONE_BOND_STATE,
+    },
+    "C": {
+        "N1": "Nsb",
+        "C2": "Cdb",
+        "O2": "Odb",
+        "N3": "Ndb",
+        "C4": "Cdb",
+        "N4": "Nsb",
+        "C5": "Cres",
+        "C6": "Cdb",
+        "H": "H",
+        **RNA_BACKBONE_BOND_STATE,
+    },
+    "G": {
+        "N1": "Nsb",
+        "C2": "Cdb",
+        "N2": "Nsb",
+        "N3": "Ndb",
+        "C4": "Cdb",
+        "C5": "Cres",
+        "C6": "Cdb",
+        "O6": "Odb",
+        "N7": "Ndb",
+        "C8": "Cdb",
+        "N9": "Nsb",
+        "H": "H",
+        **RNA_BACKBONE_BOND_STATE,
+    },
+    "T": {
+        "N1": "Nsb",
+        "C2": "Cdb",
+        "O2": "Odb",
+        "N3": "Nsb",
+        "C4": "Cdb",
+        "O4": "Odb",
+        "C5": "Cres",
+        "C": "Csb",  # TODO check this? Maybe incorrect
+        "C6": "Cdb",
+        "H": "H",
+        **RNA_BACKBONE_BOND_STATE,
+    },
+    "U": {
+        "N1": "Nsb",
+        "C2": "Cdb",
+        "O2": "Odb",
+        "N3": "Nsb",
+        "C4": "Cdb",
+        "O4": "Odb",
+        "C5": "Cres",
+        "C6": "Cdb",
+        "H": "H",
+        **RNA_BACKBONE_BOND_STATE,
+    },
+}
+"""List of atoms mapped to bondstates from Heyrovska 2008."""
