8329f9be2ebf3f668e0f35664caa340db21e345c
Arian Jamasb <arjamasb@gmail.com>
2022-08-02 06:58:04 +0200

    try changing import order in datasets init

--- graphein/ml/datasets/__init__.py
+++ graphein/ml/datasets/__init__.py
@@ -1,11 +1,11 @@
+from .torch_geometric_dataset import (
+    InMemoryProteinGraphDataset,
+    ProteinGraphDataset,
+    ProteinGraphListDataset,
+)
 from .proteins_x import (
     ProteinsLigands,
     ProteinsMetal,
     ProteinsNucleic,
     ProteinsNucleotides,
-)
-from .torch_geometric_dataset import (
-    InMemoryProteinGraphDataset,
-    ProteinGraphDataset,
-    ProteinGraphListDataset,
 )
