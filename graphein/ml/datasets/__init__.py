5d10045be0e41bd95b09dcad38a0b5acf603b16a
a-r-j <arjamasb@gmail.com>
2022-08-02 04:01:23 +0200

    add ProteinsX dataset classes

--- graphein/ml/datasets/__init__.py
+++ graphein/ml/datasets/__init__.py
@@ -1,3 +1,9 @@
+from .proteins_x import (
+    ProteinsLigands,
+    ProteinsMetal,
+    ProteinsNucleic,
+    ProteinsNucleotides,
+)
 from .torch_geometric_dataset import (
     InMemoryProteinGraphDataset,
     ProteinGraphDataset,
