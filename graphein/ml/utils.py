62eb5bfa659d7a0728fbcfb39cd775cbcca5f749
a-r-j <arjamasb@gmail.com>
2022-08-06 10:59:18 +0200

    add data combiner util

--- graphein/ml/utils.py
+++ graphein/ml/utils.py
@@ -8,6 +8,20 @@
 from typing import Any, List
 
 import networkx as nx
+from loguru import logger as log
+
+from graphein.utils.utils import import_message
+
+try:
+    from torch_geometric.data import Data
+except ImportError:
+    message = import_message(
+        "graphein.ml.utils",
+        "torch_geometric",
+        pip_install=True,
+        conda_channel="pyg",
+    )
+    log.warning(message)
 
 
 def add_labels_to_graph(
@@ -27,3 +41,24 @@ def add_labels_to_graph(
     for i, g in enumerate(graphs):
         g.graph[name] = labels[i]
     return graphs
+
+
+def combine_pyg_data(interactor_a: Data, interactor_b: Data) -> Data:
+    """
+    Combines two pytorch geometric Data objects into a single object. This is
+    useful in paired input problems, e.g. protein-protein interaction.
+
+    Attributes stored in each interactor are given prefixes in the returned
+    object. E.g.: ``interactor_a.x`` becomes ``output.a_x` (similarly we will
+    also have: ``output.b_x``.
+
+    :param interactor_a: The first interactor.
+    :type interactor_a: Data
+    :param interactor_b: The second interactor.
+    :type interactor_b: Data
+    """
+    a = interactor_a.to_dict()
+    b = interactor_b.to_dict()
+    a = {f"a_{k}": v for k, v in a.items()}
+    b = {f"b_{k}": v for k, v in b.items()}
+    return Data().from_dict({**a, **b})
