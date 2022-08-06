2db8ce2c3540dd5eac1dcecd942de2fd54f5b9cb
a-r-j <arjamasb@gmail.com>
2022-08-06 12:43:37 +0200

    try to fix docker bug by not importing prefab datasets

--- graphein/ml/datasets/__init__.py
+++ graphein/ml/datasets/__init__.py
@@ -1,9 +1,3 @@
-from .proteins_x import (
-    ProteinsLigands,
-    ProteinsMetal,
-    ProteinsNucleic,
-    ProteinsNucleotides,
-)
 from .torch_geometric_dataset import (
     InMemoryProteinGraphDataset,
     ProteinGraphDataset,
