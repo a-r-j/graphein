58584e852e08252f8046fd5db58f76da23003d17
Arian Jamasb <arjamasb@gmail.com>
2022-12-21 13:07:43 +0000

    add tests for tensor parsing

--- tests/protein/tensor/test_io.py
+++ tests/protein/tensor/test_io.py
@@ -7,7 +7,10 @@
 
 from biopandas.pdb import PandasPdb
 
-from graphein.protein.tensor.io import protein_df_to_chain_tensor
+from graphein.protein.tensor.io import (
+    protein_df_to_chain_tensor,
+    protein_df_to_tensor,
+)
 
 
 def get_example_df():
@@ -35,3 +38,18 @@ def test_protein_df_to_chain_tensor():
     ), "Number of residues and chain IDs do not match."
     assert chain.max() == num_chains - 1, "Chain IDs are not zero-indexed."
     assert chain.min() == 0, "Chain IDs are not zero-indexed."
+
+
+def test_protein_df_to_tensor():  # sourcery skip: extract-duplicate-method
+    df = get_example_df()
+
+    positions = protein_df_to_tensor(df)
+    assert positions.shape[0] == len(df), "Incorrect number of residues."
+    assert positions.shape[1] == 37, "Incorrect number of atoms."
+    assert positions.shape[2] == 3, "Incorrect number of coordinates."
+
+    # Backbone only
+    atoms_to_keep = ["N", "CA", "C", "O"]
+    assert positions.shape[0] == len(df), "Incorrect number of residues."
+    assert positions.shape[1] == 4, "Incorrect number of atoms."
+    assert positions.shape[2] == 3, "Incorrect number of coordinates."
