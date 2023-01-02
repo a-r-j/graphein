c52acc4228f2529220c391e7431a2bebd6a38b1a
Arian Jamasb <arjamasb@gmail.com>
2023-01-02 16:21:55 +0000

    add io tests

--- tests/protein/tensor/test_io.py
+++ tests/protein/tensor/test_io.py
@@ -11,10 +11,11 @@ from graphein.protein.tensor.io import (
     protein_df_to_chain_tensor,
     protein_df_to_tensor,
 )
+from graphein.protein.tensor.sequence import get_residue_id
 
 
 def get_example_df():
-    p = PandasPdb().fetch_pdb("3ED8")
+    p = PandasPdb().fetch_pdb("3EIY")
     return p.df["ATOM"]
 
 
@@ -22,19 +23,23 @@ def test_protein_df_to_chain_tensor():
     df = get_example_df()
     num_chains = len(df.chain_id.unique())
 
+    num_residues = len(get_residue_id(df))
+
     chains = protein_df_to_chain_tensor(df, one_hot=True)
-    assert chains.shape[0] == len(
-        df
+    print(chains)
+    assert (
+        chains.shape[0] == num_residues
     ), "Number of residues and chain IDs do not match."
     assert (
         chains.shape[1] == num_chains
     ), "Number of chains do not match dimension"
-    assert chains.max() == num_chains - 1, "Chain IDs are not zero-indexed."
-    assert chains.min() == 0, "Chain IDs are not zero-indexed."
+    assert chains.max() == 1, "Chain IDs are not one hot."
+    if num_chains > 1:
+        assert chains.min() == 0, "Chain IDs are not one hot."
 
     chain = protein_df_to_chain_tensor(df, one_hot=False)
-    assert chain.shape[0] == len(
-        df
+    assert (
+        chain.shape[0] == num_residues
     ), "Number of residues and chain IDs do not match."
     assert chain.max() == num_chains - 1, "Chain IDs are not zero-indexed."
     assert chain.min() == 0, "Chain IDs are not zero-indexed."
@@ -43,14 +48,18 @@ def test_protein_df_to_chain_tensor():
 def test_protein_df_to_tensor():  # sourcery skip: extract-duplicate-method
     df = get_example_df()
 
+    num_residues = len(get_residue_id(df))
+    print(get_residue_id(df))
+    assert num_residues == 174, "Incorrect number of residues."
+
     positions = protein_df_to_tensor(df)
-    assert positions.shape[0] == len(df), "Incorrect number of residues."
+    assert positions.shape[0] == num_residues, "Incorrect number of residues."
     assert positions.shape[1] == 37, "Incorrect number of atoms."
     assert positions.shape[2] == 3, "Incorrect number of coordinates."
 
     # Backbone only
     atoms_to_keep = ["N", "CA", "C", "O"]
     positions = protein_df_to_tensor(df, atoms_to_keep=atoms_to_keep)
-    assert positions.shape[0] == len(df), "Incorrect number of residues."
+    assert positions.shape[0] == num_residues, "Incorrect number of residues."
     assert positions.shape[1] == 4, "Incorrect number of atoms."
     assert positions.shape[2] == 3, "Incorrect number of coordinates."
