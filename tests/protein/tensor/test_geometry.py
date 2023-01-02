8b9750b2cbd5078bb3f84aab505e89f5b4ebd908
Arian Jamasb <arjamasb@gmail.com>
2023-01-02 16:22:06 +0000

    add geometry tests

--- tests/protein/tensor/test_geometry.py
+++ tests/protein/tensor/test_geometry.py
@@ -1,10 +1,19 @@
+"""Tests for graphein.protein.tensor.geometry."""
+# Graphein
+# Author: Arian Jamasb <arian@jamasb.io>
+# License: MIT
+# Project Website: https://github.com/a-r-j/graphein
+# Code Repository: https://github.com/a-r-j/graphein
+
 import numpy as np
 import torch
 
 from graphein.protein.tensor.geometry import (
     angle_to_unit_circle,
     dihedrals_to_rad,
+    quaternion_to_matrix,
     torsion_to_rad,
+    whole_protein_kabsch,
 )
 
 
@@ -34,3 +43,41 @@ def test_dihedrals_to_rad():
 
     delta = ((delta + 2 * np.pi) / np.pi) % 2
     np.testing.assert_allclose(delta, torch.zeros_like(delta), atol=1e-5)
+
+
+def test_whole_protein_kabsch():
+    # Compute the RMSD between the original and aligned coordinates
+    def rmsd(x, y):
+        return torch.sqrt(torch.mean((x - y) ** 2))
+
+    # Test 2D
+    A = torch.rand(10, 2)
+    R0 = torch.tensor(
+        [[np.cos(60), -np.sin(60)], [np.sin(60), np.cos(60)]],
+        dtype=torch.float,
+    )
+    B = (R0.mm(A.T)).T
+    t0 = torch.tensor([3.0, 3.0])
+    B += t0
+    R, t = whole_protein_kabsch(A, B, return_rot=True)
+    A_aligned = (R.mm(A.T)).T + t
+    error = rmsd(A_aligned, B)
+    assert error < 1e-6, "RMSD too high after alignment"
+
+    # Test 3D
+    coords = torch.rand(10, 3)
+    # Create a random rotation matrix
+    quat = torch.rand(4)
+    rot = quaternion_to_matrix(quat)
+    # Apply a random rotation and translation to coords
+    coords_distorted = rot.mm(coords.T).T + torch.rand(3) * 4
+    r, t = whole_protein_kabsch(coords_distorted, coords, return_rot=True)
+    coords_aligned = r.mm(coords_distorted.T).T + t
+
+    assert rmsd(coords_aligned, coords) < 1e-6, "RMSD too high after alignment"
+    assert torch.allclose(
+        coords_aligned, coords, atol=1e-6
+    ), "Coords differ too much after alignment"
+
+
+test_whole_protein_kabsch()
