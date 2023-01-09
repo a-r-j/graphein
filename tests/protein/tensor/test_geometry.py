ae0f4833a82ea34129dcc7492c2bc4121ddb51ee
Arian Jamasb <arjamasb@gmail.com>
2023-01-09 16:08:22 +0000

    fix kabsch test

--- tests/protein/tensor/test_geometry.py
+++ tests/protein/tensor/test_geometry.py
@@ -9,6 +9,7 @@ import numpy as np
 import pytest
 
 from graphein.protein.tensor.geometry import (
+    center_protein,
     quaternion_to_matrix,
     whole_protein_kabsch,
 )
@@ -19,6 +20,12 @@ try:
     TORCH_AVAIL = True
 except ImportError:
     TORCH_AVAIL = False
+
+
+def test_center_protein():
+    x = torch.randn((5, 3))
+    x_centered = center_protein(x)
+    assert torch.allclose(x_centered.mean(dim=0), torch.zeros(3), atol=1e-5)
 
 
 @pytest.mark.skipif(not TORCH_AVAIL, reason="PyTorch not available")
@@ -36,7 +43,7 @@ def test_whole_protein_kabsch():
     B = (R0.mm(A.T)).T
     t0 = torch.tensor([3.0, 3.0])
     B += t0
-    R, t = whole_protein_kabsch(A, B, return_rot=True)
+    R, t = whole_protein_kabsch(A, B, return_transformed=False)
     A_aligned = (R.mm(A.T)).T + t
     error = rmsd(A_aligned, B)
     assert error < 1e-6, "RMSD too high after alignment"
@@ -48,7 +55,9 @@ def test_whole_protein_kabsch():
     rot = quaternion_to_matrix(quat)
     # Apply a random rotation and translation to coords
     coords_distorted = rot.mm(coords.T).T + torch.rand(3) * 4
-    r, t = whole_protein_kabsch(coords_distorted, coords, return_rot=True)
+    r, t = whole_protein_kabsch(
+        coords_distorted, coords, return_transformed=False
+    )
     coords_aligned = r.mm(coords_distorted.T).T + t
 
     assert rmsd(coords_aligned, coords) < 1e-6, "RMSD too high after alignment"
