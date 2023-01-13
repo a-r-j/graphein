1a051dbaa6699de1d15f28636495541e7e804665
Arian Jamasb <arjamasb@gmail.com>
2023-01-13 13:54:25 +0000

    update docstrings

--- graphein/protein/tensor/testing.py
+++ graphein/protein/tensor/testing.py
@@ -26,9 +26,9 @@ except ImportError:
 
 
 def has_nan(x: torch.Tensor) -> bool:
-    """Checks a tensor for NaN values.
+    """Checks a tensor for ``NaN`` values.
 
-    :param x: Tensor to check
+    :param x: Tensor to check for ``NaN`` values
     :type x: torch.Tensor
     :return: Boolean indicating whether the tensor contains NaN values.
     :rtype: bool
@@ -50,19 +50,26 @@ def has_complete_backbone(
     a complete backbone assignment.
 
     If ``fill_value`` entries are found in the ``backbone_indices`` (dim 1) then
-    the backbone is not considered complete and we return False. If no entries with
-    the ``fill_value`` are found, we return true.
+    the backbone is not considered complete and we return ``False``. If no 
+    entries with the ``fill_value`` are found, we return ``True``.
 
-    :param x: Atom Tensor to check
+    :param x: Atom Tensor to check backbone completeness.
     :type x: graphein.protein.tensor.types.AtomTensor
     :param fill_value: Fill value used in the ``AtomTensor``. Default is ``1e-5``.
     :type fill_value: Float
     :param backbone_indices: List of indices in dimension 1 of the AtomTensor
-        to be checked. Defaults to ``[0, 1, 2, 3]`` for ``N, Ca, C, O`` in the
+        to be checked. Defaults to ``[0, 1, 2, 3]`` for ``[N, Ca, C, O]`` in the
         default assignment.
     :type backbone_indices: List[int].
     :return: Boolean indicating whether the backbone is complete.
     :rtype: bool
+
+    .. seealso:: 
+    
+        :func:`graphein.protein.tensor.testing.has_complete_residue`
+        :func:`graphein.protein.tensor.testing.is_complete_structure`
+
+
     """
     indices = torch.tensor(backbone_indices)
     return torch.sum(x[:, indices] == fill_value) == 0
@@ -80,8 +87,14 @@ def has_complete_residue(
     :param fill_value: Fill value used to denote the absence of an atom,
         defaults to ``1e-5``.
     :type fill_value: float, optional
-    :return: Boolean indicating whether the residue has all of the requisite atoms.
+    :return: Boolean indicating whether the residue has all of the requisite
+        atoms.
     :rtype: bool
+
+    .. seealso:: 
+    
+        :func:`graphein.protein.tensor.testing.has_complete_residue`
+        :func:`graphein.protein.tensor.testing.is_complete_structure`
     """
     if len(residue_type) == 1:
         residue_type = STANDARD_AMINO_ACID_MAPPING_TO_1_3[residue_type]
@@ -103,17 +116,26 @@ def has_complete_residue(
 
 
 def is_complete_structure(
-    x: AtomTensor, residues: Union[List[str], str]
+    x: AtomTensor,
+    residues: Union[List[str], str],
+    fill_value: float = 1e-5
 ) -> bool:
     """Checks whether a protein structure has all of the requisite atoms.
 
-    :param x: AtomTensor to check
+    :param x: AtomTensor to check for completeness.
     :type x: graphein.protein.tensor.types.AtomTensor
     :param residues: List of 1 or 3-letter codes of residue types.
     :type residues: Union[List[str], str]
+    :param fill_value: Fill value used to denote the absence of an atom,
+        defaults to ``1e-5``.
+    :type fill_value: float, optional
     :return: Boolean indicating whether the structure has all of the requisite
         atoms.
     :rtype: bool
+
+    .. seealso::
+        
+        :func:`graphein.protein.tensor.sequence.infer_residue_types`
     """
     length = x.shape[0]
     assert (
@@ -123,14 +145,20 @@ def is_complete_structure(
     if isinstance(residues, str):
         residues = list(residues)
 
-    return all(has_complete_residue(x[i], residues[i]) for i in range(length))
+    return all(has_complete_residue(
+        x[i], residues[i], fill_value=fill_value
+        ) for i in range(length))
 
 
 def random_atom_tensor(length: int = 64) -> AtomTensor:
     """Returns a random tensor of shape ``length x 37 x 3``.
 
 
-    .. seealso:: :meth:`random_coord_tensor`
+    .. seealso:: 
+    
+        :func:`random_coord_tensor`
+        :func:`graphein.protein.tensor.data.get_random_protein`
+        :func:`graphein.protein.tensor.data.get_random_batch`
 
     :param length: Length of random protein, defaults to ``64``
     :type length: int, optional
@@ -143,7 +171,10 @@ def random_atom_tensor(length: int = 64) -> AtomTensor:
 def random_coord_tensor(length: int = 64) -> CoordTensor:
     """Returns a random tensor of shape ``length x 3``.
 
-    .. seealso:: :meth:`random_atom_tensor`
+    .. seealso:: 
+        :func:`random_atom_tensor`
+        :func:`graphein.protein.tensor.data.get_random_protein`
+        :func:`graphein.protein.tensor.data.get_random_batch`
 
     :param length: Length of random coordinates, defaults to ``64``
     :type length: int, optional
