a41eabdbdd7aa574bf30d222e0f5aa245d1459d4
Ramon Viñas Torné <rvinast@gmail.com>
2023-06-28 09:27:04 +0200

    Retrieve mapping PDB <-> RFAM family via FTM. PDBManager-like interface

--- graphein/rna/download_rfam.py
+++ graphein/rna/download_rfam.py
@@ -1,127 +1,135 @@
-from typing import List, Optional
+import gzip
+import os
+import shutil
+from pathlib import Path
 
 import pandas as pd
-import requests
-from tqdm import tqdm
+import wget
 
-# RFAM API endpoint to retrieve family information
-RFAM_API_URL = "https://rfam.org/family"
+from loguru import logger as log
 
 
-class FamilyNotFound(ValueError):
-    pass
+class RFAMManager:
+    """ A utility for downloading RFAM families and their PDB structure IDs."""
 
+    def __init__(
+        self,
+        root_dir: str = ".",
+    ):
+        # Arguments
+        self.root_dir = Path(root_dir)
 
-def _get_RFAM_family_df(family_id: str):
-    """
-    Downloads DataFrame of PDB IDs annotated with RFAM families
-    :param family_id: RFAM ID
-    :type family_id: str
-    :return: Pandas DataFrame with information about the structures of the RFAM family
-    :rtype: pd.DataFrame
-    """
-    # Send an HTTP GET request to retrieve the data
-    response = requests.get(
-        f"{RFAM_API_URL}/{family_id}/structures?content-type=application/json"
-    )
-
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        # Extract the mapping from response
-        data = response.json()
-        df = pd.DataFrame(data["mapping"])
-    else:
-        raise FamilyNotFound(
-            f"Error occurred while retrieving family <-> structures mapping for family {family_id} (status code: {response.status_code})"
+        # Constants
+        self.rfam_families_url = "https://ftp.ebi.ac.uk/pub/databases/Rfam/CURRENT/database_files/family.txt.gz"
+        self.rfam_pdb_mapping_url = (
+            "https://ftp.ebi.ac.uk/pub/databases/Rfam/CURRENT/Rfam.pdb.gz"
         )
 
-    return df
+        self.rfam_dir = self.root_dir / "rfam"
+        if not os.path.exists(self.rfam_dir):
+            os.makedirs(self.rfam_dir)
 
+        self.rfam_families_archive_filename = Path(self.rfam_families_url).name
+        self.rfam_families_filename = Path(self.rfam_families_url).stem
+        self.rfam_pdb_mapping_archive_filename = Path(
+            self.rfam_pdb_mapping_url
+        ).name
+        self.rfam_pdb_mapping_filename = Path(self.rfam_pdb_mapping_url).stem
 
-def _get_RFAM_family_info(family_id: str, keys: Optional[List[str]] = None):
-    """
-    Downloads DataFrame of PDB IDs annotated with RFAM families
-    :param family_id: RFAM ID
-    :type family_id: str
-    :param keys: List of keys from the family endpoint to annotate (https://docs.rfam.org/en/latest/api.html#family)
-    :type keys: Optional[List[str]]
-    :return: Pandas DataFrame with information about the structures of the RFAM family
-    :rtype: pd.DataFrame
-    """
-    if keys is None:
-        keys = ["id", "description"]
+        self.download_metadata()
 
-    # Send an HTTP GET request to retrieve the data
-    response = requests.get(
-        f"{RFAM_API_URL}/{family_id}?content-type=application/json"
-    )
+    def download_metadata(self):
+        """ Download metadata mapping PDB structures to RFAM families """
+        self._download_rfam_families()
+        self._download_rfam_pdb_mapping()
 
-    # Check if the request was successful (status code 200)
-    out = {}
-    if response.status_code == 200:
-        # Extract the family information from the response
-        data = response.json()
-        for k in keys:
-            out[k] = data["rfam"][k]
-    else:
-        raise FamilyNotFound(
-            f"Error occurred while retrieving data for family {family_id} (status code: {response.status_code})"
+    def _download_rfam_families(self):
+        """Download RFAM families from
+        https://ftp.ebi.ac.uk/pub/databases/Rfam/CURRENT/database_files/family.txt.gz
+        """
+        if not os.path.exists(self.rfam_dir / self.rfam_families_filename):
+            log.info("Downloading RFAM families...")
+            wget.download(self.rfam_families_url, out=str(self.rfam_dir))
+            log.info("Downloaded RFAM families")
+
+        # Unzip all collected families
+        if not os.path.exists(self.rfam_dir / self.rfam_families_filename):
+            log.info("Unzipping RFAM sequences...")
+            with gzip.open(
+                self.rfam_dir / self.rfam_families_archive_filename, "rb"
+            ) as f_in:
+                with open(
+                    self.rfam_dir / self.rfam_families_filename, "wb"
+                ) as f_out:
+                    shutil.copyfileobj(f_in, f_out)
+            log.info("Unzipped RFAM families")
+
+    def _download_rfam_pdb_mapping(self):
+        """Download RFAM families from
+        https://ftp.ebi.ac.uk/pub/databases/Rfam/CURRENT/database_files/Rfam.pdb.gz
+        """
+        if not os.path.exists(self.rfam_dir / self.rfam_pdb_mapping_filename):
+            log.info("Downloading RFAM family - PDB structure ID mapping ...")
+            wget.download(self.rfam_pdb_mapping_url, out=str(self.rfam_dir))
+            log.info("Downloaded RFAM family - PDB structure ID mapping")
+
+        # Unzip all collected mappings
+        if not os.path.exists(self.rfam_dir / self.rfam_pdb_mapping_filename):
+            log.info("Unzipping RFAM family - PDB structure ID mapping...")
+            with gzip.open(
+                self.rfam_dir / self.rfam_pdb_mapping_archive_filename, "rb"
+            ) as f_in:
+                with open(
+                    self.rfam_dir / self.rfam_pdb_mapping_filename, "wb"
+                ) as f_out:
+                    shutil.copyfileobj(f_in, f_out)
+            log.info("Unzipped RFAM family - PDB structure ID mapping")
+
+    def _parse_rfam_families(self) -> pd.DataFrame:
+        """Parse the RFAM families metadata
+
+        :return: Pandas DataFrame with information about the RFAM families
+        :rtype: pd.DataFrame
+        """
+        df = pd.read_csv(
+            self.rfam_dir / self.rfam_families_filename,
+            sep="\t",
+            header=None,
+            encoding="ISO-8859-1",
         )
+        # Selecting accession, ID, and description
+        df = df[
+            [0, 1, 3]
+        ]  # TODO: Could select other fields such as comment for an extended description of the family?
+        df.columns = ["rfam_acc", "id", "description"]
+        df = df.set_index("rfam_acc")
+        return df
 
-    return out
+    def _parse_rfam_pdb_mapping(self) -> pd.DataFrame:
+        """Parse the PDB IDs annotated with RFAM families
 
+        :return: Pandas DataFrame with information about the structures of the RFAM family
+        :rtype: pd.DataFrame
+        """
+        df = pd.read_csv(
+            self.rfam_dir / self.rfam_pdb_mapping_filename, sep="\t", header=0
+        )
+        return df
 
-def RFAM_families_df(
-    family_ids: Optional[List[str]] = None,
-    max_id: int = 4236,
-    family_info_keys: Optional[List[str]] = None,
-    verbose=True,
-):
-    """
-    Retrieves a DataFrame of PDB IDs annotated with RFAM families
-    :param family_ids: List of families to retrieve. If None, retrieves all families: RF00001, RF00002, ..., RF04236
-    (we assume that RFAM family IDs are in increasing order, see: http://http.ebi.ac.uk/pub/databases/Rfam/CURRENT/fasta_files/)
-    :type family_ids: Optional[List[str]]
-    :param max_id: Maximum identifier to try. If family_ids is None, it'll query RFAM families RF00001 .. f'RF{max_id:05d}'
-    :type max_id: int
-    :param family_info_keys: List of keys from the family endpoint to annotate (https://docs.rfam.org/en/latest/api.html#family)
-    :type family_info_keys: Optional[List[str]]
-    :param verbose: Whether to print messages
-    :type verbose: bool
-    :return: Pandas DataFrame with information about the structures of each RFAM family
-    :rtype: pd.DataFrame
-    """
-    if family_ids is None:
-        family_ids = [f"RF{i:05d}" for i in range(1, max_id + 1)]
-
-    families_df = None
-    if verbose:
-        print("Retrieving RFAM families ...")
-    for family_id in tqdm(family_ids):
-        # Retrieve DF for a single family
-        try:
-            # Get family <-> structures mapping
-            df = _get_RFAM_family_df(family_id)
-
-            # Annotate family with descriptions
-            out = _get_RFAM_family_info(family_id, keys=family_info_keys)
-            for k, v in out.items():
-                df[k] = v
-
-            # Concatenate
-            if families_df is None:
-                families_df = df
-            else:
-                families_df = pd.concat([families_df, df])
-        except FamilyNotFound as e:
-            if verbose:
-                print(e)
-            continue
-    return families_df
+    def parse_rfam(self) -> pd.DataFrame:
+        """Parse mapping between PDB structures and RFAM families """
+        family_info_df = self._parse_rfam_families()
+        rfam_pdb_mapping_df = self._parse_rfam_pdb_mapping()
+        df = pd.merge(
+            rfam_pdb_mapping_df,
+            family_info_df,
+            left_on="rfam_acc",
+            right_index=True,
+        )
+        return df
 
 
 if __name__ == "__main__":
-    family_IDs = None  # ['RF10000']
-    families_df = RFAM_families_df(family_IDs)
-    print(families_df)
-    # families_df.to_csv('RFAM_families_27062023.csv', index=False)
+    rfam_manager = RFAMManager()
+    df = rfam_manager.parse_rfam()
+    print(df.head())
