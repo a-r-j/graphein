fb1d42dceaf22af5ecec5b54a10296bba52ce7e9
Chenyang Gong <64294860+Linsastar@users.noreply.github.com>
2024-03-02 11:02:43 +0800

    Update distance.py
    Corrected the error of not distinguishing between hydrogen receptors and donors, distinguishing OE as OE1 and OE2

--- graphein/protein/edges/distance.py
+++ graphein/protein/edges/distance.py
@@ -336,6 +336,22 @@ def add_disulfide_interactions(
 def add_hydrogen_bond_interactions(
     G: nx.Graph, rgroup_df: Optional[pd.DataFrame] = None
 ):
+    ACCEPTORS_ATOM: List[str] = ["ND",  # histidine and asparagine
+                             "NE",  # glutamate, tryptophan, arginine, histidine
+                             "NH",  # arginine
+                             "NZ",  # lysine
+                             "OH",
+                             "SD",  # cysteine
+                             "SG",  # methionine
+                             "OG",
+                             "N",
+                             ]
+    DONORS_ATOM: List[str] = ["OD1",
+                          "OD2",
+                          "OE1",
+                          "OE2",
+                          "O",
+                          ]
     """Add all hydrogen-bond interactions."""
     # For these atoms, find those that are within 3.5A of one another.
     if rgroup_df is None:
@@ -348,7 +364,8 @@ def add_hydrogen_bond_interactions(
         "NZ",  # lysine
         "OD1",
         "OD2",
-        "OE",
+        "OE1",
+        "OE2",
         "OG",
         "OH",
         "SD",  # cysteine
@@ -356,6 +373,7 @@ def add_hydrogen_bond_interactions(
         "N",
         "O",
     ]
+    
     hbond_df = filter_dataframe(rgroup_df, "atom_name", HBOND_ATOMS, True)
     if len(hbond_df.index) > 0:
         distmat = compute_distmat(hbond_df)
@@ -371,6 +389,21 @@ def add_hydrogen_bond_interactions(
         distmat = compute_distmat(hbond_df)
         interacting_atoms = get_interacting_atoms(4.0, distmat)
         add_interacting_resis(G, interacting_atoms, hbond_df, ["hbond"])
+    for r1, r2 in get_edges_by_bond_type(G, "hbond"):
+        # print(G.nodes.data())
+        condition1 = (
+                G.nodes[r1]["atom_type"] in ACCEPTORS_ATOM
+                and G.nodes[r2]["atom_type"] in DONORS_ATOM
+        )
+        condition2 = (
+                G.nodes[r2]["atom_type"] in ACCEPTORS_ATOM
+                and G.nodes[r1]["atom_type"] in DONORS_ATOM
+        )
+        is_ionic = condition1 or condition2
+        if not is_ionic:
+            G.edges[r1, r2]["kind"].remove("hbond")
+            if len(G.edges[r1, r2]["kind"]) == 0:
+                G.remove_edge(r1, r2)
 
 
 def add_ionic_interactions(
